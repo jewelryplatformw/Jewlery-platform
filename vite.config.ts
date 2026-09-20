e0629465cf1330f9a01662fe21b3e778e203b550
Glow Gallery <jewelry.platformw@gmail.com>
2026-09-20 03:26:28 +0100

    Remove base property from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-  base: '/',
-    import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
