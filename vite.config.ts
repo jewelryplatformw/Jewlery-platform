4c0972e1b1ca46fa52596bb64ecc98cc6ba03f1a
Glow Gallery <jewelry.platformw@gmail.com>
2026-09-20 03:31:48 +0100

    Change alias configuration format in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    alias: [
+      { find: '@', replacement: '/src' }
+    ]
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
